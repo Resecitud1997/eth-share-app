d6cbabf94b2d36b08986403318b4cd4055ac6cf5
Resecitud1997 <resecitud@gmail.com>
2026-04-20 19:06:24 -0400

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-
+export const FORWARDER_ADDRESS = "0x..."; // Dirección del MinimalForwarder
+export const REWARDS_CONTRACT_ADDRESS = "0x..."; // Tu contrato de ganancias
+export const CHAIN_ID = 1; // 1 para Ethereum Mainnet, 11155111 para Sepolia
